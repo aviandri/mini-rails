2b9fe08ccb0eb854273c13b150dec20a0d827acf
aviandri <aviandri@gmail.com>
2013-10-29 22:00:07 +0700

    implement activerecord

---
+++ lib/active_record.rb
@@ -0,0 +1,39 @@
+require "connection_adapter"
+
+module ActiveRecord
+	class Base
+		@@connection = SqlliteAdapter.new
+		def initialize(attributes = {})
+		  @attributes = attributes
+		end
+
+		def method_missing(name, *arg)
+			columns = @@connection.columns(self.class.table_name)
+			if columns.include?(name)
+				@attributes[name]
+			else
+				super
+			end
+		end
+
+		def self.find(id)
+			find_by_sql("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
+		end
+
+		def self.all
+			find_by_sql("SELECT * FROM #{table_name}")
+		end
+
+		def self.find_by_sql(sql)
+			rows = @@connection.execute(sql) # [{id: 1, name: "Avi"}]
+			rows.map do | attributes|
+				new attributes
+			end
+		end
+
+		def self.table_name
+			name.downcase + "s"
+		end
+	end
+	
+end
